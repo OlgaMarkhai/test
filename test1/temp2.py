baffb4f88f2e804c41eedd459e9c8d3bb74d4557
OlgaMarkhai <neola@inbox.ru>
2021-05-26 20:49:09 +0300

    third change

--- test1/temp2.py
+++ test1/temp2.py
@@ -2,6 +2,7 @@ import numpy as np
 
 number = str(np.random.randint(1, 101))  # загаданное число
 print("Загадано число от 1 до 100")
+print("Загадано число от 1 до 100")
 predict = int(input("введите число"))  # предполагаемое число
 
 def binary_search(number, predict):
